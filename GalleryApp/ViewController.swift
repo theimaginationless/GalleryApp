97fc80886e40462da511b6d878fde80a3c704609
Dmitry Teplyakov <dimon2242@gmail.com>
2021-02-12 05:18:42 +0300

    Initial Commit

---
+++ GalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Dmitry Teplyakov on 12.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
